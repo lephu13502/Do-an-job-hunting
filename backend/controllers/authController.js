ca613e155564c80877f3d421505888589da2053d
gibanguyen <101050182+gibanguyen@users.noreply.github.com>
2022-08-05 20:44:55 +0700

    Login Register update

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,10 +1,11 @@
-const User = require('../models/User')
+const Candidate = require('../models/Candidate')
+const jwt = require('jsonwebtoken')
 
 const authController = {
     // REGISTER
     registerUser: async(req, res) => {
         try {
-            const newUser = await new User({
+            const newUser = await new Candidate({
                 username: req.body.username,
                 password: req.body.password,
                 email: req.body.email
@@ -18,12 +19,21 @@ const authController = {
     },
     loginUser: async(req, res) => {
         try {
-            const user = await User.findOne({ username: req.body.username })
+            const user = await Candidate.findOne({ username: req.body.username })
             if (!user) {
                 res.status(404).json('Wrong username!')
             } else if (req.body.password !== user.password) {
                 res.status(404).json('Wrong password!')
-            } else res.status(200).json(user)
+            } else {
+                const accessToken = jwt.sign(
+                    {
+                        id: user.id
+                    },
+                    process.env.JWT_ACCESS_KEY,
+                    { expiresIn: "24h"}
+                )
+                res.status(200).json({user, accessToken})
+            }
         }catch(error) {
             res.status(500).json(error)
         }
