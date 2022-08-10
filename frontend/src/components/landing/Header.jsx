fb3c3c79512bc5d2cb260189980a10d87cc8e57e
CornCornn <88552189+Cornfufu@users.noreply.github.com>
2022-08-10 22:05:26 +0700

    Update homepage

--- frontend/src/components/landing/Header.jsx
+++ frontend/src/components/landing/Header.jsx
@@ -3,17 +3,16 @@ import {Typography} from '@material-tailwind/react';
 export default function Header() {
     return (
         <div className="relative pt-16 pb-32 flex content-center items-center justify-center h-screen">
-            <div className="bg-landing-background bg-cover bg-center absolute top-0 w-full h-full" />
+            <div className="bg-landing-background blur-sm bg-cover bg-center absolute top-0 w-full h-full" />
             <div className="container max-w-8xl relative mx-auto">
                 <div className="items-center flex flex-wrap">
                     <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-                        <Typography className="text-white text-3xl font-semibold">
-                            Contact Us
+                        <Typography className="text-black text-4xl font-extrabold ">
+                            Tìm việc khó có Ajob lo
                         </Typography>
                         <div className="text-gray-200">
-                            <Typography className="text-white text-sm">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Maecenas eget nisl id libero tincidunt sodales.
+                            <Typography className="whitespace-pre-line text-black text-xs font-bold">
+                            AJobDayRoi là một trang web giúp bạn tìm việc dễ dàng hơn, tiện lợi hơn
                             </Typography>
                         </div>
                     </div>
